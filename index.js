a756fd8db11c6a9fd9d7b2c73283839c25282dd3
rsheridan613 <rsheridan613@gmail.com>
2022-11-26 18:40:07 -0700

    added html file for help formatting generate file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateHTML = require("./utils/generateHTML");
 
+// Questions
+// Questions asked when initiating file
 const managerQuestions = [
   {
     name: "teamName",
@@ -18,6 +20,7 @@ const managerQuestions = [
   { name: "officeNum", type: "input", message: "What is your office number?" },
 ];
 
+// Questions asked when engineer option is chosen
 const engineerQuestions = [
   { name: "name", type: "input", message: "What is this person's name?" },
   { name: "id", type: "input", message: "What is this person's ID?" },
@@ -29,6 +32,7 @@ const engineerQuestions = [
   },
 ];
 
+//Questions asked when intern option is chosen
 const internQuestions = [
   { name: "name", type: "input", message: "What is this person's name?" },
   { name: "id", type: "input", message: "What is this person's ID?" },
@@ -40,9 +44,20 @@ const internQuestions = [
   },
 ];
 
+// Question asked after each prompt ends
 const roleQuestion = {
   name: "role",
   type: "list",
   message: "Which role would you like to add next?",
   choices: ["Engineer", "Intern", "Finish"],
 };
+
+// Functions
+function init() {
+  inquirer.prompt(managerQuestions).then(
+    (response) => console.log(response)
+    //writeToFile("custom.html", generateHTML(responce))
+  );
+}
+
+init();
